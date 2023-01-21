b97336bc985e844ba6d183deff20a95f9c8172b1
kazuki13 <92904203+kazuki13@users.noreply.github.com>
2023-01-21 18:23:43 +0900

    aa

--- project/main.py
+++ project/main.py
@@ -1,16 +1,18 @@
 from cgi import test
-from flask import render_template, Flask, Response, Blueprint
+from flask import render_template, Flask, Response, Blueprint, request
 import cv2
 import numpy as np
 import os 
+from flask_sqlalchemy import SQLAlchemy
+from .models import User
 app = Flask(__name__)
 main = Blueprint('main', __name__)
 print(cv2.__version__)
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, login_user
 # For each person, enter one numeric face id
 
 
-
+@login_required
 def picture():
     recognizer = cv2.face.LBPHFaceRecognizer.create()
     recognizer.read('trainer/trainer.yml')
@@ -21,6 +23,7 @@ def picture():
     #iniciate id counter
     id = 0
     count = 0
+    
     # names related to ids: example ==> Marcelo: id=1,  etc
     names = ['None', 'kazuki', 'Paula', 'Ilza', 'Z', 'W'] 
 
@@ -32,9 +35,9 @@ def picture():
     # Define min window size to be recognized as a face
     minW = 0.1*cam.get(3)
     minH = 0.1*cam.get(4)
-
+    Path = input("PATHを入力してください")
     while True:
-
+       
         ret, img =cam.read()
         #img = cv2.flip(img, -1) # Flip vertically
 
@@ -56,10 +59,14 @@ def picture():
             # Check if confidence is less them 100 ==> "0" is perfect match 
             if (confidence < 100):
                 id = names[id]
-                confidence = "  {0}%".format(round(100 - confidence))
-                count = count + 1
-                if count == 5:
-                    break
+                
+                print(id)
+                if Path == id:
+                    confidence = "  {0}%".format(round(100 - confidence))
+                    count = count + 1
+                else:
+                    id = "unknown"
+                    confidence = "  {0}%".format(round(100 - confidence))
             else:
                 id = "unknown"
                 confidence = "  {0}%".format(round(100 - confidence))
@@ -84,17 +91,21 @@ def picture():
     print("\n [INFO] Exiting Program and cleanup stuff")
     cam.release()
     cv2.destroyAllWindows()
+    return render_template(user=current_user.name)
+    
 
 
 
 
-@main.route('/video_feed')
+@main.route('/video_feed',methods=['GET'])
+@login_required
 def video_feed():
    #imgタグに埋め込まれるResponseオブジェクトを返す
    return Response(picture(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@main.route('/')
+@main.route('/', methods=['POST'])
+@login_required
 def index2():
     return render_template('index.html')
 
@@ -102,10 +113,12 @@ def index2():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
-@main.route('/index')
+@main.route('/index', methods=['GET'])
+@login_required
 def index():
    
    user = {'username': 'FZ50'}
-   return render_template('index2.html', title='home', user=user)
+   return render_template('index2.html', title='home', user=current_user.name)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
